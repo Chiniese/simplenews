9b95265239e33e8a7d0fa43f1b660bcabbfc9306
Aoyihala <1214504624@qq.com>
2017-09-11 18:37:32 +0800

    新闻实体
    单条新闻实体

---
+++ app/src/main/java/com/p2p/dsad/news/bean/DataBean.java
@@ -0,0 +1,118 @@
+package com.p2p.dsad.news.bean;
+
+/**
+ *
+ * Created by dsad on 2017/9/11.
+ */
+
+class DataBean {
+    /**
+     * news_id : 68883
+     * title : 广西边防查获夜鹭132只：眼睛被戳瞎 嘴巴被封住
+     * top_image : http://cms-bucket.nosdn.127.net/d88f03b799ca4772ae9127fc0833a63b20170911100938.png
+     * text_image0 : http://cms-bucket.nosdn.127.net/71a0aad8a81f440fa89654852e776a1420170911100938.png
+     * text_image1 : http://cms-bucket.nosdn.127.net/fbc37cce6efc423a85b3237c1339615320170911100938.png
+     * source : 中国新闻网
+     * content :
+
+
+
+     "图为被戳瞎眼睛的夜鹭，双眼灰白有血晕。李昱颖摄"
+     图为被戳瞎眼睛的夜鹭，双眼灰白有血晕。李昱颖摄
+
+     * digest : 中新网防城港9月11日电(张振国李昱颖)广西公安边防总队防城港边防支队横江边境检查站11日披露，该站日前查获活体夜鹭132只、中华鲎120只，死体夜鹭90公斤。
+     * reply_count : 756
+     * edit_time : 1505094840
+     */
+
+    private String news_id;
+    private String title;
+    private String top_image;
+    private String text_image0;
+    private String text_image1;
+    private String source;
+    private String content;
+    private String digest;
+    private String reply_count;
+    private String edit_time;
+
+    public String getNews_id() {
+        return news_id;
+    }
+
+    public void setNews_id(String news_id) {
+        this.news_id = news_id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTop_image() {
+        return top_image;
+    }
+
+    public void setTop_image(String top_image) {
+        this.top_image = top_image;
+    }
+
+    public String getText_image0() {
+        return text_image0;
+    }
+
+    public void setText_image0(String text_image0) {
+        this.text_image0 = text_image0;
+    }
+
+    public String getText_image1() {
+        return text_image1;
+    }
+
+    public void setText_image1(String text_image1) {
+        this.text_image1 = text_image1;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getDigest() {
+        return digest;
+    }
+
+    public void setDigest(String digest) {
+        this.digest = digest;
+    }
+
+    public String getReply_count() {
+        return reply_count;
+    }
+
+    public void setReply_count(String reply_count) {
+        this.reply_count = reply_count;
+    }
+
+    public String getEdit_time() {
+        return edit_time;
+    }
+
+    public void setEdit_time(String edit_time) {
+        this.edit_time = edit_time;
+    }
+}
